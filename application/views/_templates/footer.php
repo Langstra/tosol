978a8b5483670f84870bce60f5393190e5454b18
langstra <wybren.kortstra@gmail.com>
2014-10-09 10:56:58 +0200

    Enabled tosol design in the application

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,2 +1,20 @@
+<footer>
+    <div class="site-width">
+        <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms of Service</a></li>
+            <li><a href="#">Help</a></li>
+            <li><a href="#">Guidelines</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <p id="copyright">Copyright &copy; <a href="#">Tosol.com</a>. The Oxford School of Language 2014. All Rights Reserved. All Images are Trademark of their Respective owners.</p>
+    </div>
+</footer>
+<!-- Javascript -->
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/plugins.min.js" type="text/javascript"></script>
+<script src="js/custom.js" type="text/javascript"></script>
 </body>
 </html>
--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -1,47 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Meta Tags -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>PHP MVC skeleton</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Page Details -->
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- css -->
-    <link href="<?php echo URL; ?>public/css/style.css" rel="stylesheet">
-    <!-- jQuery -->
-    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
-    <!-- our JavaScript -->
-    <script src="<?php echo URL; ?>public/js/application.js"></script>
+    <meta name="keywords" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+    <!-- Page Title -->
+    <title>The Oxford School of Language - Dashboard</title>
+    <!-- Style Sheets -->
+    <link href="<?php echo URL; ?>public/css/adder.css" rel="stylesheet" type="text/css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-<!-- header -->
-<div class="container">
-    <!-- Info -->
-    <div class="where-are-we-box">
-        Everything in this box is loaded from <span class="bold">application/views/_templates/header.php</span> !
-        <br />
-        The green line is added via JavaScript (to show how to integrate JavaScript).
-    </div>
-    <h1>The header (used on all pages)</h1>
-    <!-- demo image -->
-    <h3>Demo image, to show usage of public/img folder</h3>
-    <div>
-        <img src="<?php echo URL; ?>public/img/demo-image.png" />
-    </div>
-    <!-- navigation -->
-    <h3>Demo Navigation</h3>
-    <div class="navigation">
-        <ul>
-            <!-- same like "home" or "home/index" -->
-            <li><a href="<?php echo URL; ?>"><?php echo URL; ?>home</a></li>
-            <li><a href="<?php echo URL; ?>home/exampleone"><?php echo URL; ?>home/exampleone</a></li>
-            <li><a href="<?php echo URL; ?>home/exampletwo"><?php echo URL; ?>home/exampletwo</a></li>
-            <!-- "songs" and "songs/index" are the same -->
-            <li><a href="<?php echo URL; ?>songs/"><?php echo URL; ?>songs/index</a></li>
+<!-- Top Bar -->
+<nav id="main-nav">
+    <div class="site-width">
+        <!-- Links -->
+        <ul id="main-links">
+            <li class="active"><a href="#">Dashboard</a></li>
+            <li><a href="#">Grammar</a></li>
+            <li><a href="#">Common Phrases</a></li>
+            <li><a href="#">Activity</a></li>
+            <li><a href="#">Community</a></li>
         </ul>
+        <!-- User Info -->
+        <div id="user-info-nav">
+            <ul>
+                <!-- Dropdown -->
+                <li>
+                    <a href="#"><i class="fa fa-bullseye"></i></a>
+
+                    <div class="topbar-dropdown">
+                        <div class="inner">
+                            Content Bro
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-bar-chart"></i></a>
+
+                    <div class="topbar-dropdown">
+                        <div class="inner">
+                            Content Bro
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">
+                        <i class="fa fa-bell"></i></a>
+
+                    <div class="topbar-dropdown">
+                        <div class="inner">
+                            Content Bro
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">iPhoenixi <i class="fa fa-angle-down"></i></a></li>
+            </ul>
+        </div>
     </div>
-    <!-- simple div for javascript output, just to show how to integrate js into this MVC construct -->
-    <h3>Demo JavaScript</h3>
-    <div id="javascript-header-demo-box">
-    </div>
-</div>
+</nav>
