89a23c35497710259384eeaa4aca336ad4ec7131
Langstra <wybren.kortstra@gmail.com>
2014-12-09 00:36:11 +0100

    Better code visibility

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -8,9 +8,19 @@
 
 class User extends Controller {
 
-    public function register()
+
+    private $user;
+    public $registration_error;
+    public $login_error;
+
+    function __construct()
     {
         $this->user = $this->loadModel("User");
+    }
+
+    public function register()
+    {
+
 
         if(isset($_POST['csrf']) && $this->csrf->checkToken()) {
             $this->registration_error = $this->user->register($_POST);
@@ -26,6 +36,15 @@ class User extends Controller {
     }
 
     public function login() {
+        if(isset($_POST['csrf']) && $this->csrf->checkToken()) {
+            $this->login_error = $this->user->login($_POST);
+            if($this->login_error === true) {
+                unset($this->login_error);
+            }
+        } else {
+            $this->login_error[] = array('field' => "csrf", 'rule' => 'validate_token');
+        }
+
         require VIEWS_PATH . '_templates/hp_header.php';
         require VIEWS_PATH . 'user/login.php';
         require VIEWS_PATH . '_templates/hp_footer.php';
