957eee2ba64f4b01faf803426b66e4dcf3f29ec5
Langstra <wybren.kortstra@gmail.com>
2014-12-08 05:22:20 +0100

    Created login view

---
+++ application/views/user/login.php
@@ -0,0 +1,18 @@
+<section id="login-section">
+    <div class="site-width">
+        <form id="login-form">
+            <ul id="login-alt">
+                <li><a href="#" class="facebook"><i class="fa fa-facebook"></i> Login with Facebook</a></li>
+                <li><a href="#" class="googleplus"><i class="fa fa-google-plus"></i> Login with Facebook</a></li>
+            </ul>
+            <label for="email">Enter Email</label>
+            <input type="email" name="email" id="email" placeholder="Email">
+            <label for="password">Enter Password</label>
+            <input type="password" name="password" id="password" placeholder="Password">
+            <input type="checkbox" name="" id="labelname" />
+            <label for="labelname">Remember Me</label><br>
+            <input type="submit" name="submit" value="Login">
+            <br><a href="#">Forgot Your Password?</a>
+        </form>
+    </div>
+</section>
